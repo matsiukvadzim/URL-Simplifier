f09c50f5c0c0a67a06201d4e4c39d1ab0f020bcd
Nagibator228 <withexciting@gmail.com>
2018-03-05 17:25:49 +0300

    Initial commit

---
+++ src/main/java/simplifier/UrlSimplifierApplication.java
@@ -0,0 +1,12 @@
+package simplifier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlSimplifierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrlSimplifierApplication.class, args);
+	}
+}
