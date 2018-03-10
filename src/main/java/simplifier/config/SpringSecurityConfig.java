21bce5ee8d36d28426d1350cb2c6590be78aac4a
Vadzim Matsiuk <matsiukvadzim@gmail.com>
2018-03-10 17:20:47 +0300

    fix 403 forbidden

--- src/main/java/simplifier/config/SpringSecurityConfig.java
+++ src/main/java/simplifier/config/SpringSecurityConfig.java
@@ -17,5 +17,8 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
   @Override
   protected void configure(HttpSecurity http) throws Exception {
     http.authorizeRequests().antMatchers("/").permitAll();
+
+    http.csrf().disable();
+    http.headers().frameOptions().disable();
   }
 }
