7f15ccc1cc1b964a5c0292a01bea10f526550528
Vadzim Matsiuk <matsiukvadzim@gmail.com>
2018-03-11 13:26:36 +0300

    added integration test

---
+++ src/test/java/simplifier/UserControllerIT.java
@@ -0,0 +1,39 @@
+package simplifier;
+
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+import simplifier.model.User;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT )
+public class UserControllerIT {
+
+  private TestRestTemplate restTemplate = new TestRestTemplate();
+
+  @Autowired
+  public void setRestTemplate(TestRestTemplate restTemplate) {
+    this.restTemplate = restTemplate;
+  }
+
+  @Test
+  public void createUser() {
+   User user = new User();
+   user.setUsername("test");
+   user.setPassword("test");
+
+   ResponseEntity<User> responseEntity = restTemplate.postForEntity("/user",
+       user, User.class);
+
+   assertEquals(HttpStatus.CREATED, responseEntity.getStatusCode());
+
+  }
+}
